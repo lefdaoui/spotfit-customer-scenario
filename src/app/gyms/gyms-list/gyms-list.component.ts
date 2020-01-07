1746aae021639eae1f446333a1cf2c033437dd49
b.mamatahir <b.mamatahir@spotfit.com>
2020-01-07 12:33:40 +0100

    general: break

--- src/app/gyms/gyms-list/gyms-list.component.ts
+++ src/app/gyms/gyms-list/gyms-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Gym} from '../shared/models/gym';
+import {GYMS} from '../shared/data/gyms';
 
 @Component({
   selector: 'app-gyms-list',
@@ -6,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gyms-list.component.scss']
 })
 export class GymsListComponent implements OnInit {
+  gyms: Gym[] = GYMS;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
